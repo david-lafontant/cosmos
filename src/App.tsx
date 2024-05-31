78355a34f5a9b20dc6ee1d0fb676866eeda9c5fc
David Lafontant <lafontant.david@gmail.com>
2024-05-31 17:27:25 -0400

    Import and display Header  in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.scss'
+import Header from './components/header/Header'
 
 function App() {
 
 
   return (
     <React.Fragment>
+      <Header />
 
     </React.Fragment>
   )
