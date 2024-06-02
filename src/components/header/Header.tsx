d2e9953581264b32818e72c1de71269d471b1b59
David Lafontant <lafontant.david@gmail.com>
2024-06-02 19:17:10 -0400

    Create basic responsive header and navbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,29 @@
-
+import { LuMenuSquare } from "react-icons/lu";
 
 const Header = () => {
+  function handleClick() {
+    const menuToggle = document.getElementById("check") as HTMLInputElement;
+    if (menuToggle) {
+      menuToggle.checked = false;
+    }
+  }
+
   return (
-    <header className="header">
-      <h1>Hello</h1>
-      <h2>World</h2>
+    <header id="header">
+      <nav>
+        <input type="checkbox" id="check" />
+        <label htmlFor="check" className="checkbtn">
+          <LuMenuSquare className="menu" />
+        </label>
+        <label className="logo">COSMOS</label>
+        <ul id="navlist">
+          <li><a className="active" href="#" onClick={handleClick}>Home</a></li>
+          <li><a href="#" onClick={handleClick}>About</a></li>
+          <li><a href="#" onClick={handleClick}>Contact</a></li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default Header;
