fdac10119cb230f7e8ae55c3f37a63c1ebae7f15
David Lafontant <lafontant.david@gmail.com>
2024-06-02 20:05:55 -0400

    Import and display all the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
-import './App.scss'
-import Header from './components/header/Header'
+import React from 'react';
+import './App.scss';
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
 
 function App() {
 
@@ -8,9 +11,11 @@ function App() {
   return (
     <React.Fragment>
       <Header />
-
+      <Home />
+      <About />
+      <Contact />
     </React.Fragment>
-  )
+  );
 }
 
 export default App
