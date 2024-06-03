be1b59028e343dd62ecfeeeb8e13eb0ca9219e35
David Lafontant <lafontant.david@gmail.com>
2024-06-03 17:00:37 -0400

    Update pages import

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/header/Header';
-import Home from './pages/home/Home';
-import About from './pages/about/About';
-import Contact from './pages/contact/Contact';
-import NoPage from './pages/nopage/NoPage';
+import { About, Contact, Home, NoPage } from './pages/index'
 
 function App() {
 
