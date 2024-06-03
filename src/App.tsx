b974461b90a94a0727cb27d9a68d5280cf7f6cc4
David Lafontant <lafontant.david@gmail.com>
2024-06-03 19:44:09 -0400

    Set route for dynamic content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
-import Layout from './components/layout/Layout';
+import { Astra, Layout } from './components/index';
 import { About, Contact, Home, NoPage } from './pages/index'
+
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
+            <Route path="/astra/:slug" element={<Astra />} />
             <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
