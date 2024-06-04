34cf0ef47caf84b77715305bd5f3413ab8ab7f7d
David Lafontant <lafontant.david@gmail.com>
2024-06-04 08:11:25 -0400

    Retrieve the data from the store and display it

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,36 @@
 import arr from "../../utils/card";
+
+import { useGetTodayApodQuery } from '../../services/apod';
 import Card from "../../components/card/Card";
 import Hero from "../../components/hero/Hero";
 
 
 const Home = () => {
-
+  const { data, error, isLoading } = useGetTodayApodQuery('');
 
   return (
     <main className="pages">
+
+
+
+
+
       <section className="banner">
         <h1>Welcome to Cosmos</h1>
       </section>
-      {arr[0] ? <Hero data={arr[0]} /> : null}
+
+      {error ? (
+        <h1>Oh no, there was an error</h1>
+      ) : isLoading ? (
+        <h1>Loading...</h1>
+      ) : data ? (
+        <article>
+          <p>************************</p>
+          <br />
+          <Hero data={data} />
+
+        </article>
+      ) : null}
       <section className="cards">
         {arr && arr.length > 0 ? arr.map((elem, index) => <Card key={index} data={elem} />) : null}
       </section>
