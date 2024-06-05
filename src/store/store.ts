73e6bf2e10dcbceccc1b6e2d025cdffcd4b390e3
David Lafontant <lafontant.david@gmail.com>
2024-06-04 22:14:37 -0400

    Import apodsApi in Store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { apodApi } from '../services/apod';
+import { apodsApi } from '../services/apods';
 
 export const store = configureStore({
   reducer: {
     [apodApi.reducerPath]: apodApi.reducer,
+    [apodsApi.reducerPath]: apodsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apodApi.middleware),
+    getDefaultMiddleware().concat(apodApi.middleware, apodsApi.middleware),
 })
 
 setupListeners(store.dispatch);
