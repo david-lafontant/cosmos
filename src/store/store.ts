c841c1079b823bbcbd6e452a54ee26e77a54bfbb
David Lafontant <lafontant.david@gmail.com>
2024-06-06 09:21:37 -0400

    Remove apodApi in reducer and middleware to clean store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
-import { apodApi } from '../services/apod';
 import { apodsApi } from '../services/apods';
 
 export const store = configureStore({
   reducer: {
-    [apodApi.reducerPath]: apodApi.reducer,
     [apodsApi.reducerPath]: apodsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apodApi.middleware, apodsApi.middleware),
-})
+    getDefaultMiddleware().concat(apodsApi.middleware),
+})	
 
 setupListeners(store.dispatch);
