a9aec2e42d674b53ee6f2744d1dc5c723c1519de
David Lafontant <lafontant.david@gmail.com>
2024-06-06 09:14:32 -0400

    Add getTodayApod as endpoint

--- src/services/apods.ts
+++ src/services/apods.ts
@@ -5,12 +5,15 @@ const apiKey = import.meta.env.VITE_API_NASA_API;
 
 export const apodsApi = createApi({
   reducerPath: 'apodsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=8` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `https://api.nasa.gov/planetary` }),
   endpoints: (builder) => ({
-    getApods: builder.query<Apods, string>({
-      query: (name) => `${name}`,
+   getApods: builder.query<Apods, number>({
+      query: (quantity) => `/apod?api_key=${apiKey}&count=${quantity}`,
     }),
-  }),
+        getTodayApod: builder.query<Apod, void>({
+      query: () => `/apod?api_key=${apiKey}`,
+    }),
+}),
 });
 
-export const { useGetApodsQuery } = apodsApi;
+export const { useGetApodsQuery , useGetTodayApodQuery} = apodsApi;
