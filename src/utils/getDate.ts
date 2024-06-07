2985efc1fe2b723f92af9146cfd9106ec698ac4b
David Lafontant <lafontant.david@gmail.com>
2024-06-06 22:16:49 -0400

    Create a function to return today date and the date 1 year before

---
+++ src/utils/getDate.ts
@@ -0,0 +1,13 @@
+
+// Get today date and the 
+
+export function getDate(){
+  const today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth();
+  let day = today.getDate();
+  const before = new Date(year - 1, month, day);
+  const todayDate = today.toISOString().split('T')[0];
+  const oneYearago = before.toISOString().split('T')[0]
+  return {oneYearago, todayDate };
+}
