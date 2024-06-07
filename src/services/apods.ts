26959f907400ff7aa6d922e3642dfd62babb5a0c
David Lafontant <lafontant.david@gmail.com>
2024-06-06 22:18:28 -0400

    Modify getApods endpoint to fetch apods for one year ago to today

--- src/services/apods.ts
+++ src/services/apods.ts
@@ -1,19 +1,22 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type { Apods } from '../utils/types';
+import {getDate} from '../utils/getDate';
 const apiKey = import.meta.env.VITE_API_NASA_API;
+const {oneYearago, todayDate } = getDate();
 
 export const apodsApi = createApi({
   reducerPath: 'apodsApi',
   baseQuery: fetchBaseQuery({ baseUrl: `https://api.nasa.gov/planetary` }),
   endpoints: (builder) => ({
-   getApods: builder.query<Apods, number>({
-      query: (quantity) => `/apod?api_key=${apiKey}&count=${quantity}`,
+   getApods: builder.query<Apods, void>({
+      query: (quantity) => `/apod?api_key=${apiKey}&start_date=${oneYearago}&end_date=${todayDate}`,
     }),
         getTodayApod: builder.query<Apod, void>({
       query: () => `/apod?api_key=${apiKey}`,
     }),
 }),
 });
+
 
 export const { useGetApodsQuery , useGetTodayApodQuery} = apodsApi;
