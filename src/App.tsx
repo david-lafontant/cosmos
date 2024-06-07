d3e3357a6a140fbb1eb3246aa42d75bf3d6eac10
David Lafontant <lafontant.david@gmail.com>
2024-06-06 23:06:04 -0400

    Replace Contact by Gallery in import link and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import { Astra, Layout } from './components/index';
-import { About, Contact, Home, NoPage } from './pages/index'
+import { About, Gallery, Home, NoPage } from './pages/index'
+
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
+            <Route path="gallery" element={<Gallery />} />
             <Route path="/astra/:slug" element={<Astra />} />
             <Route path="*" element={<NoPage />} />
           </Route>
