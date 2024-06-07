6387878b31afff6f0171d8f69b423f93b83e0739
David Lafontant <lafontant.david@gmail.com>
2024-06-06 22:29:53 -0400

    Rename page from Contact to Gallery

--- src/pages/contact/Contact.tsx
+++
@@ -1,9 +0,0 @@
-const Contact = () => {
-  return (
-    <main className="pages">
-      <h2>Contact page</h2>
-    </main>
-  );
-}
-
-export default Contact;
---
+++ src/pages/gallery/Gallery.tsx
@@ -0,0 +1,9 @@
+const Gallery = () => {
+  return (
+    <main className="pages">
+      <h2> Gallery page</h2>
+    </main>
+  );
+}
+
+export default Gallery;
