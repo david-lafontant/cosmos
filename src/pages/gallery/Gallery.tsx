f87efba2b33925d07fd76bb1d0ad93d9429690ae
David Lafontant <lafontant.david@gmail.com>
2024-06-07 18:59:18 -0400

    Setup the masonry to layout the 2 months of apods retrived

--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -1,9 +1,17 @@
+
+import Masonry from 'react-masonry-css';
 import { useGetApodsQuery } from '../../services/apods';
 import Card from "../../components/card/Card";
 import { Apod } from '../../utils/types';
 
 const Gallery = () => {
   const { data: apods, error: apodsError, isLoading: apodsIsLoading } = (useGetApodsQuery());
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    768: 2,
+    500: 1
+  };
   return (
     <main className="pages">
       <h2> Gallery page</h2>
@@ -12,9 +20,13 @@ const Gallery = () => {
       ) : apodsIsLoading ? (
         <h1>Loading...</h1>
       ) : apods ? (
-        <section className="cards">
+        <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+        >
           {apods.map((elem: Apod, index: number) => <Card key={index} data={elem} />)}
-        </section>
+        </Masonry>
       ) : null}
     </main>
   );
