8f30a2e9f3e28eadde4d6bc9fec085c170232be7
David Lafontant <lafontant.david@gmail.com>
2024-06-06 23:05:09 -0400

    Display fetched data in Gallery

--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -1,7 +1,21 @@
+import { useGetApodsQuery } from '../../services/apods';
+import Card from "../../components/card/Card";
+import { Apod } from '../../utils/types';
+
 const Gallery = () => {
+  const { data: apods, error: apodsError, isLoading: apodsIsLoading } = (useGetApodsQuery());
   return (
     <main className="pages">
       <h2> Gallery page</h2>
+      {apodsError ? (
+        <h1>Oh no, there was an error</h1>
+      ) : apodsIsLoading ? (
+        <h1>Loading...</h1>
+      ) : apods ? (
+        <section className="cards">
+          {apods.map((elem: Apod, index: number) => <Card key={index} data={elem} />)}
+        </section>
+      ) : null}
     </main>
   );
 }
