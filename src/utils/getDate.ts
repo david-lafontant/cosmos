a7fb70d941319af341f23b0551066390d70b982a
David Lafontant <lafontant.david@gmail.com>
2024-06-08 14:48:59 -0400

    Fix eslint error (replace "let" by "const")

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -3,9 +3,9 @@
 
 export function getDate(){
   const today = new Date();
-  let year = today.getFullYear();
-  let month = today.getMonth();
-  let day = today.getDate();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const day = today.getDate();
   const before = new Date(year, month - 2, day);
   const todayDate = today.toISOString().split('T')[0];
   const oneYearago = before.toISOString().split('T')[0]
