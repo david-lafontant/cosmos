0acb384e0583099a5d1a195151049af08b69ee41
David Lafontant <lafontant.david@gmail.com>
2024-06-08 18:23:52 -0400

    Add banner class with two headings to Hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -9,6 +9,10 @@ const Hero = ({ data }: Data) => {
   return (
     <section className="hero">
       <div className="container">
+      <section className="banner">
+        <h1>Welcome to Cosmos</h1>
+        <h2>A NASA's Astronomy Picture of the Day API frontend</h2>
+      </section>
         <img src={data.hdurl} alt={data.title} id="photo" />
       </div>
     </section>
